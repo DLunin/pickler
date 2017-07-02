9713f555eb26fd84b0b862450c1f03e6656995b1
Dmitry Lunin <mail.lunin@gmail.com>
2017-07-02 20:32:34 +0300

    Added urls to setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ setup(name='pickler-magic',
       version='1.0',
       author='Dmitry Lunin',
       packages=['pickler'],
-      requires=['IPython'])
+      requires=['IPython'],
+      url='https://github.com/DLunin/pickler',
+      download_url='https://github.com/DLunin/pickler/archive/1.0.tar.gz')
