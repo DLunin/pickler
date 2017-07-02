f9b76d93414e6cbbb3f4f3d31761c144fc6d4473
Dmitry Lunin <mail.lunin@gmail.com>
2017-07-02 19:28:01 +0300

    Implemented setup script

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .pickler import *
-
-ip = get_ipython()
-ip.register_magics(PicklerMagic)
-
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pickler-magic',
+      version='1.0',
+      author='Dmitry Lunin',
+      packages=['pickler'],
+      requires=['IPython'])
